f71523abfb4a33af66474313f7f94391fae6607d
Parker Selbert <parker@sorentwo.com>
2014-10-13 00:07:26 -0500

    Add dropdown element, no event handling

---
+++ test/components/dropdown_test.js
@@ -0,0 +1,68 @@
+var expect   = require('chai').expect;
+var Dropdown = require('../../lib/components/Dropdown');
+
+describe('Dropdown', function() {
+  describe('#render', function() {
+    it('emits a list element', function() {
+      var dropdown = new Dropdown();
+      var element  = dropdown.render();
+
+      expect(element.className).to.eq('velge-dropdown');
+    });
+
+    it('lists all available choices', function() {
+      var dropdown = new Dropdown();
+      var element  = dropdown.render(['ida-red', 'gold-rush']);
+
+      var items = [].slice.call(element.querySelectorAll('li'));
+
+      expect(items).to.have.length(2);
+      expect(element.textContent).to.contain('ida-red');
+      expect(element.textContent).to.contain('gold-rush');
+    });
+
+    it('replaces choices when re-rendering', function() {
+      var dropdown = new Dropdown();
+      var element  = dropdown.element;
+
+      dropdown.render(['ida-red', 'gold-rush']);
+      dropdown.render(['jonathan']);
+
+      var items = [].slice.call(element.querySelectorAll('li'));
+
+      expect(items).to.have.length(1);
+      expect(element.textContent).to.contain('jonathan');
+    });
+
+    it('highlights a given value', function() {
+      var dropdown = new Dropdown();
+      var element  = dropdown.render(['melrose', 'suncrisp'], 'suncrisp');
+
+      var highlighted = element.querySelector('.highlighted');
+
+      expect(highlighted).to.exist;
+      expect(highlighted.textContent).to.contain('suncrisp');
+    });
+  });
+
+  describe('#open', function() {
+    it('applies an open class to the element', function() {
+      var dropdown = new Dropdown();
+
+      dropdown.open();
+
+      expect(dropdown.element.className).to.contain('open');
+    });
+  });
+
+  describe('#close', function() {
+    it('removes an open class from the element', function() {
+      var dropdown = new Dropdown();
+
+      dropdown.open();
+      dropdown.close();
+
+      expect(dropdown.element.className).not.to.contain('open');
+    });
+  });
+});
