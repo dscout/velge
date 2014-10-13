be144824963ad12f4a08e2ff8606ad1e19660ead
Parker Selbert <parker@sorentwo.com>
2014-10-12 22:52:17 -0500

    Begin specifying individual components

---
+++ test/components/wrapper_test.js
@@ -0,0 +1,20 @@
+var expect  = require('chai').expect;
+var Wrapper = require('../../lib/components/Wrapper');
+
+describe('Wrapper', function() {
+  var element;
+
+  beforeEach(function() {
+    element = document.createElement('div');
+  });
+
+  describe('#render', function() {
+    it('renders into the element', function() {
+      var component = new Wrapper({ element: element });
+
+      component.render();
+
+      expect(element.querySelector('.velge')).to.exist;
+    });
+  });
+});
